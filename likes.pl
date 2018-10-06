6a1202bcb6a5cbc5ecc30ac7bd29be7ff0f589e3
Nattapon Rakthong <nattapon.rakthong@gmail.com>
2018-10-06 19:34:56 +0700

    Add likes example

---
+++ likes.pl
@@ -0,0 +1,50 @@
+%% Demo coming from http://clwww.essex.ac.uk/course/LG519/2-facts/index_18.html
+%%
+%% Please load this file into SWI-Prolog
+%%
+%% Sam's likes and dislikes in food
+%%
+%% Considering the following will give some practice
+%% in thinking about backtracking.
+%%
+%% You can also run this demo online at
+%% http://swish.swi-prolog.org/?code=https://github.com/SWI-Prolog/swipl-devel/raw/master/demo/likes.pl&q=likes(sam,Food).
+
+/** <examples>
+?- likes(sam,dahl).
+?- likes(sam,chop_suey).
+?- likes(sam,pizza).
+?- likes(sam,chips).
+?- likes(sam,curry).
+*/
+%% san ชอบ Food ถ้า Food เป็นอาหาร indian และ Food อ่อน
+likes(sam,Food) :-
+    indian(Food),
+    mild(Food).
+
+%% san ชอบ Food ถ้า Food เป็นอาหาร chinese 
+likes(sam,Food) :-
+    chinese(Food).
+
+%% san ชอบ Food ถ้า Food เป็นอาหาร italian 
+likes(sam,Food) :-
+    italian(Food).
+
+%% san ชอบ chips 
+likes(sam,chips).
+
+indian(curry).
+indian(dahl).
+indian(tandoori).
+indian(kurma).
+
+mild(dahl).
+mild(tandoori).
+mild(kurma).
+
+chinese(chow_mein).
+chinese(chop_suey).
+chinese(sweet_and_sour).
+
+italian(pizza).
+italian(spaghetti).
